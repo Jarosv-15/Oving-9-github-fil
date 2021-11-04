f798d0378a9c67e7b31ba98bb3c71e0bbd42add7
sondr <sondre-solheim@hotmail.com>
2021-11-04 21:01:09 +0100

    Oving 9 e ferdig

--- Oving 9e.py
+++ Oving 9e.py
@@ -1,11 +1,17 @@
-class Norge:
+class Quiz:
     def __init__(self, sporsmal_txt, svar_alternativ, rett_svar):
         self.sporsmal_txt = sporsmal_txt
         self.svar_alternativ = svar_alternativ
         self.rett_svar = int(rett_svar)
 
     def kort_svar_tekst(self):
-        return
+        for i in range(len(self.alternatives)):
+            self.alternatives[i] = self.svar_alternativ[i].replace(f"{i}:", "")
+
+        index_svar = int(self.rett_svar)
+        svar_str = (self.svar_alternativ)[index_svar]
+
+        return f"Korrekt svar:{svar_str}"
 
     def __str__(self):
         alle_alternativ = '\n'.join(self.svar_alternativ)
@@ -18,10 +24,31 @@ class Norge:
             print("Det er rett!\n")
         else:
             print("Sorry, det er feil\n")
+objektliste = []
 
 def filinfo():
-    sporsmolfil = open("sporsmaalsfil.txt", "r")
+    sporsmaalfil = open("sporsmaalsfil.txt", "r", )
+    for linje in sporsmaalfil:
+        linjer = linje.split(":")
+        sporsmal_txt = linjer[0]
+
+        svar_alternativ = []
+        alternatives_str = linjer[2]
+        alternatives_str = alternatives_str.replace('[', '')
+        alternatives_str = alternatives_str.replace(']', '')
+        alternativer_list = alternatives_str.split(',')
+        for alternativ in alternativer_list:
+            endelig_alternativ = alternativ.strip()
+            svar_alternativ.append(endelig_alternativ)
+
+        rett_svar = linjer[1]
+
+        q = Quiz(sporsmal_txt, svar_alternativ, rett_svar)
+        objektliste.append(q)
+
+    return objektliste
 
 
 if __name__ == '__main__':
-
+    filinfo()
+    print(objektliste[0])
